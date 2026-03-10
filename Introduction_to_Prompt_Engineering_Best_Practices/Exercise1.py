5e3431b8169b946aba5aaa75560be0d06c1273cb
Neo Sathikge <neossadiki@gmail.com>
2026-03-10 15:11:21 +0200

    Added Intro to Prompt_engineering

---
+++ Introduction_to_Prompt_Engineering_Best_Practices/Exercise1.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not found from .env file.")
+
+def get_response(prompt):
+    client = OpenAI(api_key=api_key)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+    # Define the conversation messages
+    messages = [
+    {"role": "system", "content": "You are a helpful event management assistant."},
+    {"role": "user", "content": "What are some good conversation starters at networking events?"},
+    {"role": "assistant", "content": "I am preparing event for my friend marriage"}
+])
+    return response.choices[0].message.content
+response = get_response("")
+print(response)
